a4f6e7ad4b4e7106fa53334465bbe2b95a4affc7
killpathhh <jenya.sofronoff@gmail.com>
2025-10-28 16:41:57 +0300

    s

---
+++ Source/TestEditor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace TestEditor.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ Source/TestEditor/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TestEditor.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
